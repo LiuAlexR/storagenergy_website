0da40f646198e1e4935e9cd6f782d3af16f1348d
Alex Liu <alexliu3000@gmail.com>
2025-06-09 22:55:26 -0600

    START MAVEN/JAVA SPRING

---
+++ website/src/main/java/com/storagenergy/website/WebsiteApplication.java
@@ -0,0 +1,14 @@
+package com.storagenergy.website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Starting!");
+		SpringApplication.run(WebsiteApplication.class, args);
+	}
+
+}
